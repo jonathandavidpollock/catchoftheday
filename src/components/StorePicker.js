126eee62b542bb6830481fe3ca3a288ae579165d
Jonathan Pollock <jon.pollock@me.com>
2017-03-15 00:45:30 -0400

    finished video 15 with loading fish through state

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    // first grab the text from the box
+    // second we're going to forward the url
+    console.log('You changed the URL.')
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`)
+  }
+
   render() {
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         { /* JSX Comment */ }
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" ref={(input)=> {this.storeInput = input}} required placeholder="Store Name" defaultValue={getFunName()}/>
         <button type="submit">Visit Store</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes =  {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
